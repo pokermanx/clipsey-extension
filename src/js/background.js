5b350275fb6c7974c9fdfe6c69580e45bc4d0ebe
pokermanx <36159194+pokermanx@users.noreply.github.com>
2018-11-11 21:05:14 +0200

    removed popup, made controller as a background script, added icons

---
+++ src/js/background.js
@@ -0,0 +1,22 @@
+console.log('back')
+const popupClicked = tab => {
+    chrome.tabs.sendMessage(tab.id, true)
+    console.log('clicked')
+}
+
+const changeIcon = (message, sender) => {
+    if (message) {
+        chrome.browserAction.setIcon({
+            path: "src/icons/light-bulb-off48.png",
+            tabId: sender.tab.id
+        })
+    } else {
+        chrome.browserAction.setIcon({
+            path: "src/icons/light-bulb-on48.png",
+            tabId: sender.tab.id
+        })
+    }
+}
+
+chrome.browserAction.onClicked.addListener(popupClicked)
+chrome.runtime.onMessage.addListener(changeIcon)
--- src/js/shader.js
+++ src/js/shader.js
@@ -1,49 +1,71 @@
-const controller = (message, sender, xsendResponse) => {
-    message ? shadeAction() : shadeBack()
-    console.log(message, xsendResponse)
+// const $views = chrome.extension.getViews({ type: "popup" });
+
+// if ($views != []) {
+//     const data = sessionStorage.getItem('clipseyState')
+//     if (data) {
+//         chrome.runtime.sendMessage(data)
+//         console.log(data)
+//     }
+// }
+
+const controller = (message, sender) => {
+    $shaderBlock = document.getElementById('shader-block')
+    $shaderBlock ? shadeBack($shaderBlock) : shadeAction()
+    console.log(message, sender)
 }
 
 const shadeAction = () => {
     const $body = document.body;
-    const $player = document.querySelectorAll('[id^="player"]')
+    const $playerById = document.querySelectorAll('[id*="player"]');
+    const $playerByClass = document.querySelectorAll('[class*="player"]');
+    const $player = $playerById.length === 0 ? $playerByClass : $playerById;
+
     const filter = () => {
         const $filterDiv = document.createElement('div');
-        $filterDiv.id = 'shader-block'
+        $filterDiv.id = 'shader-block'  
         $filterDiv.style.width = '100%';
-        $filterDiv.style.height = '100%';
+        $filterDiv.style.height = '100%';   
         $filterDiv.style.left = '0';
         $filterDiv.style.top = '0';
         $filterDiv.style.position = 'fixed';
         $filterDiv.style.backgroundColor = '#000000';
         $filterDiv.style.opacity = '0.9';
-        $filterDiv.style.zIndex = '1000'
-    
+        $filterDiv.style.zIndex = '1100'
+
         return $filterDiv
     }
     
     const indexApply = () => {
         
         for (let el of $player){
-            el.style.zIndex = '1001';
+            el.style.zIndex = '1101';
         }
         console.log($player)
     }
     
     indexApply();
-
     //setTimeout(indexApply, 2000)
     
     $body.appendChild(filter())
-    // console.log(filter())
+
+    // console.log(filter())s
     // console.log(message)
+    // sessionStorage.setItem("clipseyState", "true")
+    chrome.runtime.sendMessage(true)
 } 
 
-const shadeBack = () => {
-    $shaderBlock = document.getElementById('shader-block')
+const shadeBack = $shaderBlock => {
     const $body = document.body;
     $body.removeChild($shaderBlock)
-    console.log("Off")
 
+    console.log("Lights on")
+    chrome.runtime.sendMessage(false)
+    // sessionStorage.setItem("clipseyState", "false")
 }
 
 chrome.runtime.onMessage.addListener(controller)
+
+// chrome.storage.sync.get('[clipseyState]', data => {
+//     chrome.runtime.sendMessage(data)
+//     console.log(data)
+// })
